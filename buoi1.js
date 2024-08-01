ca9dbfeba2344a02f01655fe856c3685046ef0e4
bxhiu <bxhieu2812205ls@gmail.com>
2024-08-01 17:35:01 +0700

    add buoi1js

---
+++ buoi1.js
@@ -0,0 +1,15 @@
+//console.log("Heloo");
+//alert('Loi');
+// var a = prompt("nhap so a");
+// var b = prompt("nhap so b");
+// console.log(a*1 + b*1);
+//bai lab
+console.log("Hello World");
+let a = 10;
+// set a value of 10
+console.log(a);
+alert(a);
+// Nhập dữ liệu
+var n = prompt("Input n: ");
+// Xuất dữ liệu ra màn hình
+document.write(n);
